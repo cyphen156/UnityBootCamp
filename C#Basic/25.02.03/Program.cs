5642cb708a5492fea9ffb627f88a547b13fe6565
Cyphen <dbdxor@gmail.com>
2025-02-03 17:26:10 +0900

    25.02.03
    Study At Academy
    C# basic
    summary : What is Function and array

--- C#Basic/25.02.03/Program.cs
+++ C#Basic/25.02.03/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace _25._02._03
+﻿using System;
+
+namespace _25._02._03
 {
     internal class Program
     {
+        private static T Multiply<T> (T lVal, T rVal)
+        {
+            T result = lVal;
+            result *= rVal;
+            return result;
+        }
+        static int Multiply(int lVal, int rVal)
+        {
+            return lVal * rVal;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int size = 5;
+            //for (int i = 0; i < size; ++i)
+            //{
+            //    for (int j = i; j < size-1; ++j)
+            //    {
+            //        Console.Write(' ');
+            //    }
+            //    for (int j = 0; j <= i; ++j)
+            //    {
+            //        Console.Write('*');
+            //    }
+            //    Console.WriteLine();
+            //}
+            //int size = 10;
+            //int[] data = new int[10];
+            //for (int i = 0; i < size; i++)
+            //{
+            //    for (int j = 0; j < size; j++)
+            //    {
+            //        data[i] = i + 1;
+            //    }
+            //}
+
+            int[,] data = new int[10, 10];
+
+            int cnt = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    data[i, j] = cnt;
+                    cnt++;
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(data[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Multiply(2.3f, 3.5f);
         }
     }
 }
