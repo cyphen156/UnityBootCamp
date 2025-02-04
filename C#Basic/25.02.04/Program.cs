ed385d0601d7fe13af6ebb9af6d6f10a5e23a3b5
Cyphen <dbdxor@gmail.com>
2025-02-04 10:06:28 +0900

    25.02.04_002 shaking
    Shaking Number

--- C#Basic/25.02.04/Program.cs
+++ C#Basic/25.02.04/Program.cs
@@ -6,6 +6,8 @@ namespace _25._02._04
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+
             int[] array = new int[52];
             //int[] printArray = new int[8];
             // 숫자배열 초기화
@@ -21,7 +23,6 @@ namespace _25._02._04
             //}
             for (int i = 0; i < 8; ++i)
             {
-                Random rand = new Random();
                 int temp = rand.Next(array.Length);
                 //for (int j = 0; j <= i; ++j)
                 //{
@@ -40,6 +41,7 @@ namespace _25._02._04
                 //}
                 
                 // 출력 여부 확인하기
+                int cnt = 0;
                 while(true)
                 {
                     // 등장 안했으니까 루프에서 나가
@@ -47,14 +49,15 @@ namespace _25._02._04
                     {
                         break;
                     }
-
+                    cnt++;
                     // 조건에 안걸리면 등장안한거니까 다시뽑아
                     temp = rand.Next(array.Length);
                 }
 
-                Console.Write(array[temp] + "\t");
+                Console.Write(array[temp] + "\t" + cnt);
                 // 등장 했으니까 값을 바꿔라
                 array[temp] = -1;
+                Console.WriteLine();
             }
             Console.WriteLine();
         }
---
+++ C#Basic/25.02.04/RandomShake.cs
@@ -0,0 +1,46 @@
+﻿namespace _25._02._04
+{
+    internal class RandomShake
+    {
+        // 메인 아이디어는 쉐이킹
+        // 52개를 전체를 다 섞는다면 최악의 경우 초기화 52 + 자리교체 52 + 8회 뽑기
+        // 눈속수 자리교체 52+8+8
+        // 더 줄일 방법 여쭤보자
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int[] array = new int[52];
+            // 숫자배열 초기화
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = i + 1;
+            }
+            // 전체 자리교체 코드
+            //for (int i = 0; i < array.Length; ++i)
+            //{
+            //    int idx = rand.Next(array.Length);
+            //    int temp = array[idx];
+            //    array[idx] = array[i];
+            //    array[i] = temp;
+            //}
+
+            // 눈속임 코드
+            ///**어차피 출력은 8개만 하니까 8개만 바꿔놔도 다른건 확인 못한다
+            for (int i = 0; i < 8; ++i)
+            {
+                int idx = rand.Next(array.Length);
+                int temp = array[idx];
+                array[idx] = array[i];
+                array[i] = temp;
+            }
+
+            // 출력 코드 
+            for (int i = 0; i < 8; ++i)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
