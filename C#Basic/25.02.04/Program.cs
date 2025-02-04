331118612b5121d1a9d85959e9f0418a4585aa40
Cyphen <dbdxor@gmail.com>
2025-02-04 09:30:05 +0900

    25.02.04_001 RandomPrint
    randomPrint

---
+++ C#Basic/25.02.04/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _25._02._04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[52];
+            //int[] printArray = new int[8];
+            // 숫자배열 초기화
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = i+1;
+            }
+            // 확인배열 초기화
+
+            //for (int i = 0; i < 8; ++i)
+            //{
+            //    printArray[i] = 0;
+            //}
+            for (int i = 0; i < 8; ++i)
+            {
+                Random rand = new Random();
+                int temp = rand.Next(array.Length);
+                //for (int j = 0; j <= i; ++j)
+                //{
+                //    // 출현했으면 다시뽑아
+                //    if (temp == printArray[j])
+                //    {
+                //        temp = rand.Next(array.Length);
+                //    }
+                //    // 처음 등장하는거면 탈출해
+                //    else
+                //    {
+                //        printArray[j] = temp;
+                //        Console.WriteLine(printArray[]);
+                //        break;
+                //    }
+                //}
+                
+                // 출력 여부 확인하기
+                while(true)
+                {
+                    // 등장 안했으니까 루프에서 나가
+                    if (array[temp]!=-1)
+                    {
+                        break;
+                    }
+
+                    // 조건에 안걸리면 등장안한거니까 다시뽑아
+                    temp = rand.Next(array.Length);
+                }
+
+                Console.Write(array[temp] + "\t");
+                // 등장 했으니까 값을 바꿔라
+                array[temp] = -1;
+            }
+            Console.WriteLine();
+        }
+    }
+}
