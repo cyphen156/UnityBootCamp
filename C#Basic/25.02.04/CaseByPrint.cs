43e85794aba5ccc3e5ebedb570ca97497163568f
Cyphen <dbdxor@gmail.com>
2025-02-04 11:38:19 +0900

    25.02.04_004 Fnc LotteryTypeCast

--- C#Basic/25.02.04/CaseByPrint.cs
+++ C#Basic/25.02.04/CaseByPrint.cs
@@ -2,7 +2,55 @@
 {
     internal class CaseByPrint
     {
-        
+        static private void LotteryTypeCast (int choose)
+        {
+            string str = "";
+
+            //int choose = 52;
+            str += "실제로 뽑힌 숫자 : " + choose + "\t";
+            if (choose < 14)
+            {
+                str += "Heart ";
+            }
+            else if (choose < 27)
+            {
+                str += "Diamond ";
+            }
+            else if (choose < 40)
+            {
+                str += "Clover ";
+            }
+            else
+            {
+                str += "Spade ";
+            }
+
+
+            // 뽑힌 카드 숫자 더하기
+            choose %= 13;
+            if (choose == 1)
+            {
+                str += "A";
+            }
+            else if (choose == 11)
+            {
+                str += "J";
+
+            }
+            else if (choose == 12)
+            {
+                str += "Q";
+            }
+            else if (choose == 0)
+            {
+                str += "K";
+            }
+            else
+            {
+                str += choose;
+            }
+            Console.Write(str);
+        }
         static void Main(string[] args)
         {
             Random rand = new Random();
@@ -14,54 +62,12 @@
                 array[i] = i + 1;
             }
 
-            string str = "";
             int size = 1;
             // 카드뽑기
             for (int i = 0; i < size; ++i)
             {
-                int choose = rand.Next(array.Length);
-                //int choose = 25;
-                if (choose < 14)
-                {
-                    str += "Heart ";
-                }
-                else if(choose < 27)
-                {
-                    str += "Diamond ";
-                }
-                else if (choose < 40)
-                {
-                    str += "Clover ";
-                }
-                else
-                {
-                    str += "Spade ";
-                }
-
-
-                // 뽑힌 카드 숫자 더하기
-                if (choose == 1)
-                {
-                    str += "A";
-                }
-                else if (choose == 11)
-                {
-                    str += "J";
-
-                }
-                else if (choose == 12)
-                {
-                    str += "Q";
-                }
-                else if (choose == 13)
-                {
-                    str += "K";
-                }
-                else
-                {
-                    str += choose % 13;
-                }
-                Console.Write(str);
+                int choose = rand.Next(array.Length) + 1;
+                LotteryTypeCast(choose);
             }
             Console.WriteLine();
         }
