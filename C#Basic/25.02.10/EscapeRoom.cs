904597801a429569d6f967a6d6b893f7652f4d82
Cyphen <dbdxor@gmail.com>
2025-02-10 18:51:16 +0900

    25.02.10
    Study At Academy
    About Object
    What is Object

---
+++ C#Basic/25.02.10/EscapeRoom.cs
@@ -0,0 +1,54 @@
+﻿namespace _25._02._10
+{
+
+
+    class World
+    {
+        int x;
+        int y;
+        public bool canPass;
+
+        public World(int wall, int floor)
+        {
+        }
+    }
+
+    class Object
+    {
+        public int x = 0; 
+        public int y = 0;
+
+        void Move()
+        {
+
+        }
+
+        void Move(int to)
+        {
+
+        }
+
+        public Object()
+        { 
+        
+        }
+    }
+    internal class EscapeRoom
+    {
+        static void Main(string[] args)
+        {
+            World[] Walls = new World[10];
+            World[] floor = new World[10];
+
+            Object player = new Object();
+            Object[] monsters = new Object[3];
+
+            int DestinationX, DestinationY;
+
+            if (player.x == DestinationX && player.y == DestinationY)
+            {
+                Console.WriteLine("Goal");
+            }
+        }
+    }
+}
---
+++ C#Basic/25.02.10/Lecture3.cs
@@ -0,0 +1,37 @@
+﻿namespace _25._02._10
+{
+
+    class Image
+    {
+        public int X;
+        public int Y;
+
+        public int Red;
+        public int Green;
+        public int Blue;
+
+        public Image(int x, int y, int red, int green, int blue)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+        }
+    }
+
+    internal class Lecture3
+    {
+        static void Main(string[] args)
+        {
+            int length = 14;
+            Image[] imgs = new Image[length];
+
+            int j = 0;
+            for (int i = 0; i < length; i++)
+            {
+                imgs[i] = new Image(j++, j++, j++, j++, j++);
+            }
+        }
+    }
+}
