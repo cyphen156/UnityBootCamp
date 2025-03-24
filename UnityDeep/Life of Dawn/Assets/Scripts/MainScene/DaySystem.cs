7e3496f20c2a235bcaa17bab8dfa6f68e149939c
Cyphen <dbdxor@gmail.com>
2025-03-24 20:19:37 +0900

    25.03.24_DaySystem
    DaySystem_001

--- UnityDeep/Life of Dawn/Assets/Scripts/MainScene/DaySystem.cs
+++ UnityDeep/Life of Dawn/Assets/Scripts/MainScene/DaySystem.cs
@@ -1,16 +1,65 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
+/// <summary>
+/// 타임 브로드캐스트 이벤터
+/// </summary>
 public class DaySystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float timeSpeed;
+    public float baseTime;
+    public float setTime;
+    public float weight;    // 가중치
+
+    public float currentTime;
+    public bool isDay;
+
+    public event Action<bool, float> OnDayNightChanged;
+    DirectionalLight dl;
+    private void Awake()
     {
-        
+        timeSpeed = 0.05f;
+        baseTime = 120f; // 2분
+        weight = 1.5f;
+        isDay = true;
+        setTime = baseTime;
+        dl = GetComponent<DirectionalLight>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(DayCycle());
+    }
+
+    IEnumerator DayCycle()
+    {
+        while (true)
+        {
+            float addTime = Time.deltaTime;
+            // 밤에는 시간이 늦게옴
+            if (isDay)
+            {
+                setTime = baseTime;
+            }
+            else
+            { 
+                setTime = baseTime * weight;
+            }
+            currentTime += addTime;
+            
+            if (currentTime >= setTime)
+            {
+                isDay = !isDay;
+                currentTime = 0;
+                OnDayNightChanged?.Invoke(isDay, weight);
+                Debug.Log($"[DaySystem] {(isDay ? "낮" : "밤")}으로 전환됨");
+            }
+
+            // 시간계는 다른 상태와 상관 없이 항상 일정하게 흘러야 한다.
+            yield return new WaitForSeconds(timeSpeed);
+        }
     }
 }
