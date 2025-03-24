c8aa260bb26fc41a1089c39a4242feaec0cf4776
Cyphen <dbdxor@gmail.com>
2025-03-24 12:27:50 +0900

    25.03.24_MultiPlex Server
    멀티 플렉싱 서버 개발

---
+++ C#Basic/25.03.24_MultiThread/Client/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Threading;
+using System.Data;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace Client
+{
+    class Program
+    {
+
+        //[][]
+        struct Packet
+        {
+            //[][]
+            public string id; //20
+            //[][]
+            public string message; //40
+        }
+
+        // 정수형 숫자
+        //short //htons
+        //int,  //htonl
+        //long  //htonll
+        //[1][2]
+
+        //[][]
+        static void Main(string[] args)
+        {
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4000);
+
+            clientSocket.Connect(listenEndPoint);
+
+            while (true)
+            {
+                string InputChat;
+                Console.Write("채팅 : ");
+                InputChat = Console.ReadLine();
+
+                Packet packet = new Packet();
+                packet.id = "융스택스";
+                packet.message = "노트북에서 Inner Host";
+                //string jsonString = "{\"id\" : \"태규\",  \"message\" : \"" + InputChat + ".\"}";
+                string jsonString = JsonConvert.SerializeObject(packet);
+                byte[] message = Encoding.UTF8.GetBytes(jsonString);
+                ushort length = (ushort)message.Length;
+
+                //길이  자료
+                //[][] [][][][][][][][]
+                byte[] lengthBuffer = new byte[2];
+                lengthBuffer = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)length));
+
+                //[][][][][][][][][][][]
+                byte[] buffer = new byte[2 + length];
+
+                Buffer.BlockCopy(lengthBuffer, 0, buffer, 0, 2);
+                Buffer.BlockCopy(message, 0, buffer, 2, length);
+
+                int SendLength = clientSocket.Send(buffer, buffer.Length, SocketFlags.None);
+
+
+
+                int RecvLength = clientSocket.Receive(lengthBuffer, 2, SocketFlags.None);
+                length = BitConverter.ToUInt16(lengthBuffer, 0);
+                length = (ushort)IPAddress.NetworkToHostOrder((short)length);
+                byte[] recvBuffer = new byte[4096];
+                RecvLength = clientSocket.Receive(recvBuffer, length, SocketFlags.None);
+
+                string JsonString = Encoding.UTF8.GetString(recvBuffer);
+
+                Console.WriteLine(JsonString);
+            }
+
+            clientSocket.Close();
+        }
+    }
+}
---
+++ C#Basic/25.03.24_MultiThread/Server/Program.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Message
+    {
+        public string message;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4000);
+
+            listenSocket.Bind(listenEndPoint);
+
+            listenSocket.Listen(10);
+
+            List<Socket> clientSockets = new List<Socket>();
+            List<Socket> checkRead = new List<Socket>();
+
+            while (true)
+            {
+                checkRead.Clear();
+                checkRead = new List<Socket>(clientSockets);
+                checkRead.Add(listenSocket);
+
+
+                //Polling
+                //[listen]
+                Socket.Select(checkRead, null, null, -1);
+
+                foreach (Socket findSocket in checkRead)
+                {
+                    if (findSocket == listenSocket)
+                    {
+                        Socket clientSocket = listenSocket.Accept();
+                        clientSockets.Add(clientSocket);
+                        Console.WriteLine($"Connect client : {clientSocket.RemoteEndPoint}");
+                    }
+                    else
+                    {
+                        try
+                        {
+                            byte[] headerBuffer = new byte[2];
+                            int RecvLength = findSocket.Receive(headerBuffer, 2, SocketFlags.None);
+                            if (RecvLength > 0)
+                            {
+                                short packetlength = BitConverter.ToInt16(headerBuffer, 0);
+                                packetlength = IPAddress.NetworkToHostOrder(packetlength);
+
+                                byte[] dataBuffer = new byte[4096];
+                                RecvLength = findSocket.Receive(dataBuffer, packetlength, SocketFlags.None);
+                                string JsonString = Encoding.UTF8.GetString(dataBuffer);
+                                Console.WriteLine(JsonString);
+
+                                JObject clientData = JObject.Parse(JsonString);
+
+                                string message = "{ \"message\" : \"" + clientData.Value<String>("message") + "\"}";
+                                byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+                                ushort length = (ushort)IPAddress.HostToNetworkOrder((short)messageBuffer.Length);
+
+                                headerBuffer = BitConverter.GetBytes(length);
+
+                                byte[] packetBuffer = new byte[headerBuffer.Length + messageBuffer.Length];
+                                Buffer.BlockCopy(headerBuffer, 0, packetBuffer, 0, headerBuffer.Length);
+                                Buffer.BlockCopy(messageBuffer, 0, packetBuffer, headerBuffer.Length, messageBuffer.Length);
+                                foreach (Socket sendSocket in clientSockets)
+                                {
+                                    int SendLength = sendSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
+                                }
+                            }
+                            else
+                            {
+                                findSocket.Close();
+                                clientSockets.Remove(findSocket);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Error 낸 놈 : {e.Message} {findSocket.RemoteEndPoint}");
+
+                            findSocket.Close();
+                            clientSockets.Remove(findSocket);
+                        }
+                    }
+
+                }
+
+                //Server 작업
+                {
+                    Console.WriteLine("서버 작업");
+                }
+
+            }
+
+            listenSocket.Close();
+        }
+    }
+}
